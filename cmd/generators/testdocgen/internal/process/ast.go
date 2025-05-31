de83571903f697111069a3c6a9b58c45f29c4eb9
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-06-01 02:41:14 +0900

    support star receiver

--- cmd/generators/testdocgen/internal/process/ast.go
+++ cmd/generators/testdocgen/internal/process/ast.go
@@ -16,6 +16,16 @@ func recvTypeName(fn *ast.FuncDecl) option.Option[string] {
 	case *ast.StarExpr:
 		if ident, ok := t.X.(*ast.Ident); ok {
 			return option.Some(&ident.Name)
+		} else {
+			if indexExpr, ok := t.X.(*ast.IndexExpr); ok {
+				if ident, ok := indexExpr.X.(*ast.Ident); ok {
+					return option.Some(&ident.Name)
+				}
+			} else if indexListExpr, ok := t.X.(*ast.IndexListExpr); ok {
+				if ident, ok := indexListExpr.X.(*ast.Ident); ok {
+					return option.Some(&ident.Name)
+				}
+			}
 		}
 	case *ast.IndexExpr:
 		if ident, ok := t.X.(*ast.Ident); ok {
