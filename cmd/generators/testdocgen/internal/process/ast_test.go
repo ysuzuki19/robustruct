a778cdfd2a0661aea359b9758df6495ed7198e96
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-06-01 16:26:36 +0900

    add test for recvTypeName

---
+++ cmd/generators/testdocgen/internal/process/ast_test.go
@@ -0,0 +1,70 @@
+package process
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/ysuzuki19/robustruct/cmd/generators/testdocgen/internal/strchain"
+	"github.com/ysuzuki19/robustruct/pkg/option"
+)
+
+func createSource(parts ...string) string {
+	return strchain.
+		FromSlice([]string{
+			"package main",
+		}).
+		Append(parts...).
+		Join("\n").
+		String()
+}
+
+func TestRecvTypeName(t *testing.T) {
+	require := require.New(t)
+
+	cases := []struct {
+		decl     string
+		receiver option.Option[string]
+	}{
+		{
+			decl:     "func Utility() {}",
+			receiver: option.None[string](),
+		},
+		{
+			decl:     "func (s Sample) Method() {}",
+			receiver: option.NewSome("Sample"),
+		},
+		{
+			decl:     "func (s *Sample) RefMethod() {}",
+			receiver: option.NewSome("Sample"),
+		},
+		{
+			decl:     "func (s Sample[T]) GenericMethod() {}",
+			receiver: option.NewSome("Sample"),
+		},
+		{
+			decl:     "func (s *Sample[T]) GenericRefMethod() {}",
+			receiver: option.NewSome("Sample"),
+		},
+	}
+
+	for _, c := range cases {
+		source := createSource(c.decl)
+
+		fset := token.NewFileSet()
+		file, err := parser.ParseFile(fset, "", source, parser.ParseComments)
+		require.NoError(err)
+
+		if len(file.Decls) != 1 {
+			require.Fail("no declarations found")
+			continue
+		}
+
+		fn, ok := file.Decls[0].(*ast.FuncDecl)
+		require.True(ok)
+		name := recvTypeName(fn)
+		require.Equal(c.receiver.UnwrapOrDefault(), name.UnwrapOrDefault())
+	}
+}
