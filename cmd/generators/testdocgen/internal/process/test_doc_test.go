c87a26e3052fc2a01dc5c4c35a91fe7ecc395862
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-06-01 20:26:06 +0900

    add test for ParseTestDoc

--- cmd/generators/testdocgen/internal/process/test_doc_test.go
+++ cmd/generators/testdocgen/internal/process/test_doc_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/ysuzuki19/robustruct/pkg/option"
 )
 
 func TestRegexps(t *testing.T) {
@@ -34,4 +35,42 @@ func TestRegexps(t *testing.T) {
 		require.False(c.re.MatchString("x"+c.line), "Must to be unmatched: `%s`", c.line)
 		require.False(c.re.MatchString("// "+c.line), "Must to be unmatched: `%s`", c.line)
 	}
+}
+
+func TestParseTestDocs(t *testing.T) {
+	require := require.New(t)
+
+	test := `
+package sample_test
+
+func TestUtility(t *testing.T) {
+	require := require.New(t)
+	// testdoc begin Utility
+	require.Equal(1, Utility(1))
+	// testdoc end
+}
+
+func TestSampleMethod(t *testing.T) {
+	require := require.New(t)
+	// testdoc begin Sample.Method
+	s := Sample{}
+	require.Equal(1, s.Method())
+	// testdoc end
+}
+	`
+
+	tds, err := ParseTestDocs(test)
+	require.NoError(err)
+	require.Equal([]TestDoc{
+		{
+			StructName: option.None[string](),
+			FuncName:   "Utility",
+			Content:    "\trequire.Equal(1, Utility(1))",
+		},
+		{
+			StructName: option.NewSome("Sample"),
+			FuncName:   "Method",
+			Content:    "\ts := Sample{}\n\trequire.Equal(1, s.Method())",
+		},
+	}, tds)
 }
