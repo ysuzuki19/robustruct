c63db7e4a03e52a8be04f5f7a12059c04e18f419
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-07-10 02:12:20 +0900

    update example/for_linter

--- example/for_linter/main.go
+++ example/for_linter/main.go
@@ -1,5 +1,26 @@
 package main
 
+type Color string
+
+const (
+	Red   Color = "red"
+	Green Color = "green"
+	Blue  Color = "blue"
+)
+
+func (c Color) Code() string {
+	switch c {
+	case Red:
+		return "FF0000"
+	case Green:
+		return "00FF00"
+	case Blue:
+		return "0000FF"
+	default:
+		return "000000"
+	}
+}
+
 type Human struct {
 	Name string
 	Age  int
